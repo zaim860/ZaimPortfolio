b978678e5d37a7b85a4a45a9e1b33ee2dd12c35f
bru <ploiezaim@gmail.com>
2024-06-29 23:35:07 +0800

    updated some css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,18 +10,21 @@ export const Contact = () => {
         <p>Click on my email to contact me.</p>
       </div>
       <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={getImageUrl('contact/emailIcon.png')} alt="emailicon" />
-          <a href='mailto:zaimfadzly@gmail.com'>zaimfadzly@gmail.com</a>
-        </li> 
-        <li className={styles.link}>
-          <img src={getImageUrl('contact/linkedinIcon.png')} alt="linkedinIcon" />
-          <a href='https://www.linkedin.com/in/za-im-fadzly-2a6691262/'>https://www.linkedin.com/in/za-im-fadzly-2a6691262/</a>
-        </li> 
-        <li className={styles.link}>
-          <img src={getImageUrl('contact/githubIcon.png')} alt="githubIcon" />
-          <a href='https://github.com/zaim860'>https://github.com/zaim860</a>
-        </li> 
+
+      <li className={styles.link}>
+        <img src={getImageUrl('contact/emailIcon.png')} alt="emailicon" />
+        <a href='mailto:zaimfadzly@gmail.com' target="_blank" rel="noopener noreferrer"><span>zaimfadzly@gmail.com</span></a>
+      </li> 
+      <li className={styles.link}>
+        <img src={getImageUrl('contact/linkedinIcon.png')} alt="linkedinIcon" />
+        <a href='https://www.linkedin.com/in/za-im-fadzly-2a6691262/' target="_blank" rel="noopener noreferrer"><span>Za'im Fadzly</span></a>
+      </li> 
+      <li className={styles.link}>
+        <img src={getImageUrl('contact/githubIcon.png')} alt="githubIcon" />
+        <a href='https://github.com/zaim860' target="_blank" rel="noopener noreferrer"><span>https://github.com/zaim860</span></a>
+      </li> 
+
+
       </ul>
     </footer>
   )
